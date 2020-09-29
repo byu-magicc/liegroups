85e5f1848910597d7883f9aafc424c8083e379db
Mark Petersen <markiepetersen@gmail.com>
2020-09-29 17:03:57 -0600

    started s03

--- build/test/googletest-download/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mark/projects/liegroups/build/test/googletest-download/googletest-src/googletest
-# Build directory: /home/mark/projects/liegroups/build/test/googletest-download/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
